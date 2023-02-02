75fc6f05c9b1ab94f1dc7e2dc1191256a85f5ef1
parsarezaee <parsarezaee777@yahoo.com>
2023-02-02 18:10:04 +0330

    Add Authentication for user

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,12 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 
-
-class UserAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'email']
+class UserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email', 'is_staff', 'is_active']
+    list_filter = ['email', 'is_staff', 'is_active']
+    fieldsets = (
+            (None, {"fields": ("email", "password")}),
+            ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
+        )    
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "first_name", "last_name", "email", "password1", "password2", "is_staff",
+                "is_active", "groups", "user_permissions"
+            )}
+        ),
+    )
     search_fields = ['email']
-
+    ordering = ['email']
     
     
 admin.site.register(CustomUser, UserAdmin)
