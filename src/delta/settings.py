86b0f03c9e8a9635690c2229cdbc71d39379d1ca
parsarezaee <parsarezaee777@yahoo.com>
2023-02-09 02:52:19 +0330

    Change allowed hosts

--- src/delta/settings.py
+++ src/delta/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8$lf4ug6_k5)sa%$s-eurtn@wun!l9_o9q7&*_k&e8%u^+u$)1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
